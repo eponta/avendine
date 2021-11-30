61a247f555dd63676d009c2976edfeb23a0191e2
eponta <36237788+eponta@users.noreply.github.com>
2021-11-30 02:58:36 +0100

    deploy

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -48,7 +48,8 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      
+      publicPath: process.env.NODE_ENV === "production" ? "/frontend/" : "/",
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
